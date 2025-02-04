40248c7a445e3ddb7a12496e4c17f30e2da2c9d6
Ting-Chia Chiang <t.chiang@vu.nl>
2025-02-04 18:06:07 +0100

    Disable auto start components part in init()

--- sic_framework/devices/minirobot.py
+++ sic_framework/devices/minirobot.py
@@ -9,29 +9,12 @@ from sic_framework.devices.device import SICDevice
 from sic_framework.devices.common_mini.mini_microphone import MiniMicrophone, MiniMicrophoneSensor
 from sic_framework.devices.common_mini.mini_speaker import MiniSpeaker, MiniSpeakerComponent
 
-mini_active = False
-
-def start_mini_components():
-    manager = SICComponentManager(mini_component_list,
-                                  auto_serve=False)
-    atexit.register(manager.stop)
-    from contextlib import redirect_stderr
-    with redirect_stderr(None):
-        manager.serve()
-
 
 class MiniRobot(SICDevice):
-    def __init__(self, mic_conf=None, speaker_conf=None):
-        super().__init__(ip="127.0.0.1")
+    def __init__(self, ip="127.0.0.1", mic_conf=None, speaker_conf=None):
+        super().__init__(ip=ip)
         self.configs[MiniMicrophone] = mic_conf
         self.configs[MiniSpeaker] = speaker_conf
-
-        global mini_active
-        if not mini_active:
-            # run the component manager in a thread
-            thread = threading.Thread(target=start_mini_components, name="MiniComponentManager-singelton")
-            thread.start()
-            mini_active = True
 
     @property
     def mic(self):
