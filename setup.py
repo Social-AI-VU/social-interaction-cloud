b627da1397d100017539a2fcab7f4f0a6c6eb3a0
Mike Ligthart <m.e.u.ligthart@vu.nl>
2025-07-02 09:05:15 +0200

    Bump version to 2.0.38

--- setup.py
+++ setup.py
@@ -67,7 +67,7 @@ extras_require = {
 
 setup(
     name="social-interaction-cloud",
-    version="2.0.37",
+    version="2.0.38",
     author="Koen Hindriks",
     author_email="k.v.hindriks@vu.nl",
     long_description=open("README.md").read(),
