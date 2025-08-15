6d4511b26a8bcab0a24139e1c76d1cf227e8971e
Ting-Chia Chiang <t.chiang@vu.nl>
2025-08-15 16:27:39 +0200

    Add docstrings to franka.py

--- sic_framework/devices/franka.py
+++ sic_framework/devices/franka.py
@@ -1,3 +1,9 @@
+"""
+franka.py
+
+This module provides the Franka device class and starts Franka-related components automatically.
+"""
+
 import atexit
 import threading
 
@@ -15,6 +21,10 @@ from sic_framework.devices.common_franka.franka_motion_recorder import (
 franka_active = False
 
 def start_franka_components():
+    """
+    Initialize and run the Franka component manager
+    """
+    global franka_active
     manager = SICComponentManager(franka_component_list, client_id=utils.get_ip_adress(), auto_serve=False, name="Franka")
 
     atexit.register(manager.stop)
@@ -24,7 +34,15 @@ def start_franka_components():
 
 
 class Franka(SICDevice):
+    """
+    Franka device interface that automatically starts the component manager
+    in a background thread on first initialization.
+    """
     def __init__(self, motion_conf=None):
+        """
+        Initialize the Franka device with the given motion configuration
+        :param motion_conf: Configuration for the Franka motion component
+        """
         super().__init__(ip="127.0.0.1")
         self.configs[FrankaMotion] = motion_conf
 
@@ -37,10 +55,18 @@ class Franka(SICDevice):
 
     @property
     def motion_recorder(self):
+        """
+        Get the Franka motion recorder connector.
+        :return: The FrankaMotionRecorder connector.
+        """
         return self._get_connector(FrankaMotionRecorder)
 
     @property
     def motion(self):
+        """
+        Get the Franka motion connector.
+        :return: The FrankaMotion connector.
+        """
         return self._get_connector(FrankaMotion)
 
 
