641c97e34035fc0da10ba81e0d48b1e6ad6f7b0d
sandergs92 <sandergs@protonmail.com>
2025-11-11 20:11:08 +0100

    Committing work so far of Streaming Sortformer for review

--- sic_framework/services/all_services.py
+++ sic_framework/services/all_services.py
@@ -6,7 +6,16 @@ from sic_framework.services.openai_gpt.gpt import GPTComponent
 from sic_framework.services.openai_whisper_stt.whisper_stt import (
     WhisperComponent,
 )
+from sic_framework.services.streaming_sortformer.stm_sortformer import (
+    STMSortformerComponent,
+)
 
 if __name__ == "__main__":
-
-    SICComponentManager([WhisperComponent, GPTComponent, DNNFaceDetectionComponent])
+    SICComponentManager(
+        [
+            WhisperComponent,
+            GPTComponent,
+            DNNFaceDetectionComponent,
+            STMSortformerComponent,
+        ]
+    )
